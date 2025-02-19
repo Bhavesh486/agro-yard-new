0b52f3e710f3caaa1c9cac7751ff382fc2641630
Bhavesh <sunnysonar154@gmail.com>
2025-02-19 22:01:14 +0530

    Initial Commit with the Login Fragment ui

---
+++ app/src/main/java/com/projects/agroyard/fragments/LoginUsers.java
@@ -0,0 +1,26 @@
+package com.projects.agroyard.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.projects.agroyard.R;
+
+public class LoginUsers extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_users, container, false);
+    }
+}
